9f1bf8dac6851c745683f10bf6840c9fb0df9e59
ibrahimabdu <ibrahim.abdu777@gmail.com>
2021-08-24 10:38:17 +0100

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { greet } from "./utils/greet";
 import FizzBuzz from "./components/fizzbuzz";
 
 function App(): JSX.Element {
-  return <FizzBuzz />
+  return <FizzBuzz />;
 }
 
 export default App;
--- src/components/fizzbuzz.tsx
+++ src/components/fizzbuzz.tsx
@@ -1,31 +1,26 @@
-import { JsxEmit } from "typescript"
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function FizzBuzz(): JSX.Element {
- 
-    const [count, setCount] = useState(0)
-    const [history, setHistory] = useState<any[]>([])
+  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState<(number|string)[]>([]);
 
-    function handleButtonClick() {
-        setCount(count => count +1)
-        setHistory(updateHistory)
-    }
+  function handleButtonClick() {
+    setCount((count) => count + 1);
+    setHistory(updateHistory);
+  }
 
-    function updateHistory(history: any[]) {
-        
-        if (count % 3 === 0 && count !== 0){
-            return [...history, 'fizz' ]
-        }
-        else if (count % 5 === 0 && count !== 0){
-            return [...history, 'buzz']
-        }
-        else return [...history, count]
-    }
- 
- 
-    return <>
-        <button onClick={handleButtonClick}>play fizzbuzz!</button>
-        <p>{history.join('   ')}</p>
-    
+  function updateHistory(history: (number|string)[]) {
+    if (count % 3 === 0 && count !== 0) {
+      return [...history, "fizz"];
+    } else if (count % 5 === 0 && count !== 0) {
+      return [...history, "buzz"];
+    } else return [...history, count];
+  }
+
+  return (
+    <>
+      <button onClick={handleButtonClick}>play fizzbuzz!</button>
+      <p>{history.join("   ")}</p>
     </>
+  );
 }
