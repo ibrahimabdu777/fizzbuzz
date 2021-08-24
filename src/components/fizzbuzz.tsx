e374108e130d3c1b8a5a44e427aaff70bfbe13fa
ibrahimabdu <ibrahim.abdu777@gmail.com>
2021-08-24 10:40:50 +0100

    please pass

--- src/components/fizzbuzz.tsx
+++ src/components/fizzbuzz.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 export default function FizzBuzz(): JSX.Element {
   const [count, setCount] = useState(0);
-  const [history, setHistory] = useState<(number|string)[]>([]);
+  const [history, setHistory] = useState<(number | string)[]>([]);
 
   function handleButtonClick() {
     setCount((count) => count + 1);
     setHistory(updateHistory);
   }
 
-  function updateHistory(history: (number|string)[]) {
+  function updateHistory(history: (number | string)[]) {
     if (count % 3 === 0 && count !== 0) {
       return [...history, "fizz"];
     } else if (count % 5 === 0 && count !== 0) {
